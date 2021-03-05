c7870b5493f2a274fd9c49f86543e9d4e523d07d
gwendolyntn <gwendolyntn@gmail.com>
2021-03-05 00:19:32 -0600

    base of profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Home from './pages/home/Home'
 import Login from './pages/login/Login';
+import Profile from './pages/profile/Profile'
 import ProjectDetails from './pages/project-details/ProjectDetails';
 import {
 	BrowserRouter as Router,
