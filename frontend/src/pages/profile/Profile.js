ff43a171ed1b0343cb382d253f919d9cf3b36f01
Gwen Nguyen <45651223+gwendolyntn@users.noreply.github.com>
2021-03-09 01:30:37 -0600

    Updated profile page

--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import { Container, Row, Col, Card } from 'react-bootstrap'
+import Header from "../home/components/Header"
+import Dashboard from "./components/Dashboard";
+import "../home/Home.css"
+import "./components/Dashboard.css"
 
 class Profile extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+        }
+    }
     /*Things to display on profile page */
     render(){
-        return <h1>Class Component</h1>
+        return (
+            <div className="full-screen-height">
+                <Header />
+                <Dashboard />
+            </div>
+        )  
     }
 }
 
