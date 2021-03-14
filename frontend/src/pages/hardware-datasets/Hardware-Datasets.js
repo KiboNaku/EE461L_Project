5c0a1c2f4b163d38c6a832038c7d66a436c8be04
Travis Mongoven <mongoven.t@gmail.com>
2021-03-14 12:28:46 -0500

    Beginning work on the hardware-datasets page

---
+++ frontend/src/pages/hardware-datasets/Hardware-Datasets.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { Container, Row, Col, Card } from 'react-bootstrap'
+import Header from "../home/components/Header"
+import Dashboard from "./components/Dashboard";
+import "../home/Home.css"
+import "./components/Dashboard.css"
+
+class HardwareDatasets extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+        }
+    }
+    
+    /*Things to display on hardware-datasets page */
+    render(){
+        return (
+            <div className="full-screen-height">
+                <Header />
+            </div>
+        )  
+    }
+}
+
+export default Profile
