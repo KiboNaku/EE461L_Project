33a8f32eb2949a9dc56f694314b726d4fd9648bf
Travis Mongoven <mongoven.t@gmail.com>
2021-03-17 16:08:40 -0500

    added text boxes for users to pick amounts of hardware to rent, and a button to later be used to checkout those sets

--- frontend/src/pages/hardware-datasets/Hardware-Datasets.js
+++ frontend/src/pages/hardware-datasets/Hardware-Datasets.js
@@ -10,20 +10,26 @@ class HardwareDatasets extends Component {
         this.state = {
         }
     }
+
+    checkOut(){
+        // TODO: write this function so that it accurately decrements all of the hardware sets that the user checks out,
+        // as well as checking that there are enough sets to match what the user is asking for
+    }
     
     /*Things to display on hardware-datasets page */
     render(){
         return (
             <div className="full-screen-height">
                 <Header />
-                <Card> 
+                <Card style={{margin:20}}> 
                     <Card.Header>Hardware Sets</Card.Header>
                     <Card.Body>
-                        <table class="table table-striped table-bordered">
+                        <table id="fixed-table" class="table table-striped table-bordered">
                             <thead>
                                 <tr>
-                                    <th>Name</th>
-                                    <th>Sets Available</th>
+                                    <th style={{width: 200}}>Name</th>
+                                    <th style={{width: 200}}>Sets Available</th>
+                                    <th style={{width: 150}}>Checkout Amount</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -31,32 +37,53 @@ class HardwareDatasets extends Component {
                                     <td>HWSet1</td>
                                     {/* TODO: fix this hardcoded value once profiles are implemented */}
                                     <td>10</td>
+                                    <td>
+                                        <input type="num" placeholder="Enter Number"/>
+                                        <small class="form-text text-muted">How many you would like to checkout.</small>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>HWSet2</td>
                                     {/* TODO: fix this hardcoded value once profiles are implemented */}
                                     <td>25</td>
+                                    <td>
+                                        <input type="num" placeholder="Enter Number"/>
+                                        <small class="form-text text-muted">How many you would like to checkout.</small>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>HWSet3</td>
                                     {/* TODO: fix this hardcoded value once profiles are implemented */}
                                     <td>5</td>
+                                    <td>
+                                        <input type="num" placeholder="Enter Number"/>
+                                        <small class="form-text text-muted">How many you would like to checkout.</small>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>HWSet4</td>
                                     {/* TODO: fix this hardcoded value once profiles are implemented */}
                                     <td>15</td>
+                                    <td>
+                                        <input type="num" placeholder="Enter Number"/>
+                                        <small class="form-text text-muted">How many you would like to checkout.</small>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>HWSet5</td>
                                     {/* TODO: fix this hardcoded value once profiles are implemented */}
                                     <td>50</td>
+                                    <td>
+                                        <input type="num" placeholder="Enter Number"/>
+                                        <small class="form-text text-muted">How many you would like to checkout.</small>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
                     </Card.Body>
+                    <button type="button" class="btn btn-primary btn-md" onClick="checkout()">Checkout</button>
                 </Card>
-                <Card> 
+                <Card style={{margin:20}}> 
                     <Card.Header>DataSets</Card.Header>
                     <Card.Body>
                         <table class="table table-striped table-bordered">
