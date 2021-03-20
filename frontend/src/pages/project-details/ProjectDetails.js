3f3f98b1eab67b1e828b57eece2bb4458d87d2f9
Andy Ni <nakukibo@gmail.com>
2021-03-19 20:56:06 -0500

    updated project details css

--- frontend/src/pages/project-details/ProjectDetails.js
+++ frontend/src/pages/project-details/ProjectDetails.js
@@ -13,65 +13,86 @@ class ProjectDetails extends Component {
 
     render() {
         return (
-            <div className="w-100 boring-background max-height overflow-hidden text-left">
-                <Container fluid className="justify-content-center mx-5 my-5 px-5 py-5">
+            <div className="w-100 boring-background max-height overflow-hidden text-left px-0 py-0 mx-0 my-0">
 
-                    <div className="project-title-panel my-5">
+                <div className="project-title-panel block-color-title px-5 py-5 col-5 float-left">
+                    <div className="my-4 mx-4">
                         <div className="project-name text-left">Project Name</div>
                         <div className="project-members">Owner, User 1, User 2, ...</div>
                         <div className="project-tags">
                             <p>Tags:</p>
-                            <span className="project-tag">tag1</span>    
-                            <span className="project-tag">tag2</span>    
-                            <span className="project-tag">tag3</span>    
-                            <span className="project-tag">tag4</span>    
-                            <span className="project-tag">tag5</span>    
+                            <span className="project-tag">tag1</span>
+                            <span className="project-tag">tag2</span>
+                            <span className="project-tag">tag3</span>
+                            <span className="project-tag">tag4</span>
+                            <span className="project-tag">tag5</span>
                         </div>
-                    </div>
 
-                    <div className="project-description my-3">
-                        <h4>Description:</h4>
-                        <p>Project description</p>
                     </div>
+                </div>
 
-                    {/* List of HW items */}
-                    <div className="project-materials my-3">
-                        <h4>Materials</h4>
+                {/* List of HW items */}
+                <div className="block-color-blue col-7 px-5 py-5 float-left">
+                    <table className="table borderless">
+                        <thead>
+                            <tr>
+                                <th scope="col">Checked</th>
+                                <th scope="col"></th>
+                                <th scope="col">Wishlist</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Item 1</td>
+                                <td>$20</td>
+                                <td>Wish 1</td>
+                                <td>$20</td>
+                            </tr>
+                            <tr>
+                                <td>Item 2</td>
+                                <td>$20</td>
+                                <td>Wish 2</td>
+                                <td>$20</td>
+                            </tr>
+                            <tr>
+                                <td>Item 3</td>
+                                <td>$10</td>
+                                <td>Wish 3</td>
+                                <td>$20</td>
+                            </tr>
+                            <tr>
+                                <td>Item 4</td>
+                                <td>$1</td>
+                                <td>Wish 4</td>
+                                <td>$20</td>
+                            </tr>
+                            <tr>
+                                <td>Item 5</td>
+                                <td>$1000</td>
+                                <td>Wish 5</td>
+                                <td>$20</td>
+                            </tr>
+                            <tr>
+                                <td>Item 6</td>
+                                <td>$20000</td>
+                                <td>Wish 6</td>
+                                <td>$20</td>
+                            </tr>
+                            <tr>
+                                <td className="font-weight-bold">Total:</td>
+                                <td>$20000</td>
+                                <td></td>
+                                <td>$200</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
 
-                        <div className="mx-3">
-                            {/* Checked out items */}
-                            <h5>Checked Out:</h5>
-                            <div className="mx-4">
-                                <p>Material 1</p>
-                                <p>Material 1</p>
-                                <p>Material 1</p>
-                                <p>Material 1</p>
-                                <p>Material 1</p>
-                                <p>Material 1</p>
-                                <p>Material 1</p>
-                                <p>Material 1</p>
-                            </div>
-                        </div>
-                        
-                        <div className="mx-3">
-                            {/* Wishlist items */}
-                            <h5>Wishlist:</h5>
-                            <div className="mx-4">
-                                <p>Material 1</p>
-                                <p>Material 1</p>
-                                <p>Material 1</p>
-                                <p>Material 1</p>
-                                <p>Material 1</p>
-                                <p>Material 1</p>
-                                <p>Material 1</p>
-                                <p>Material 1</p>
-                            </div>
-                        </div>
-                        <div className="mx-3">
-                            <h5>Total:</h5>
-                        </div>
-                    </div>
-                </Container>
+                <div className="project-description my-3 block-color-purple px-5 py-5">
+                    <h4>Description:</h4>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing</p>
+                </div>
             </div>
         );
     }
