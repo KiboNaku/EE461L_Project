0903f1cf5e864cc4c1c5ffb8f10c03bbed0f4d7c
Andy Ni <nakukibo@gmail.com>
2021-03-20 12:23:49 -0500

    small adjustments to project details design

--- frontend/src/pages/project-details/ProjectDetails.js
+++ frontend/src/pages/project-details/ProjectDetails.js
@@ -13,7 +13,7 @@ class ProjectDetails extends Component {
 
     render() {
         return (
-            <div className="w-100 boring-background max-height overflow-hidden text-left px-0 py-0 mx-0 my-0">
+            <div className="w-100 block-color-purple max-height overflow-hidden text-left px-0 py-0 mx-0 my-0">
 
                 <div className="project-title-panel block-color-title px-5 py-5 col-5 float-left">
                     <div className="my-4 mx-4">
@@ -89,7 +89,7 @@ class ProjectDetails extends Component {
                     </table>
                 </div>
 
-                <div className="project-description my-3 block-color-purple px-5 py-5">
+                <div className="project-description block-color-purple px-5 py-5">
                     <h4>Description:</h4>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing</p>
                 </div>
