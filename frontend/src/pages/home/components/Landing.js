0b4bc0c90774b31c26bf3b93f4052988ef2788e4
Andy Ni <nakukibo@gmail.com>
2021-03-20 13:35:32 -0500

    updated landing screen

--- frontend/src/pages/home/components/Landing.js
+++ frontend/src/pages/home/components/Landing.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+import "./Landing.css"
 
 class Landing extends Component {
 
     render() {
         return (
             <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
-                <header className="mb-auto"/>
+                <header className="mb-auto" />
 
-                <main role="main" className="inner cover">
-                    <h1 className="cover-heading">Website Name</h1>
-                    <p className="lead">Find your passion project here today.</p>
-                    <p className="lead">
-                        <a href="#" className="btn btn-lg btn-secondary">Learn more</a>
-                    </p>
+                <main role="main" className="inner cover row mx-0 my-0">
+                    <div className="cover-text mx-auto py-4">
+                        <h1 className="cover-heading">Website Name</h1>
+                        <p className="lead">Find your passion project here today.</p>
+                        <p className="lead">
+                            <a href="#" className="btn btn-lg btn-secondary">Learn more</a>
+                        </p>
+                    </div>
                 </main>
 
-                <footer className="mt-auto"/>
+                <footer className="mt-auto" />
             </div>
         )
     }
