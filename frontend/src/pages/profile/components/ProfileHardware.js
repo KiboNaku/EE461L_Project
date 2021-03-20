e9fc313fcf6645eddf9014df4c2fd360b0a7ad73
Gwen Nguyen <45651223+gwendolyntn@users.noreply.github.com>
2021-03-20 15:41:51 -0500

    fixed class to className, added billing toggle

--- frontend/src/pages/profile/components/ProfileHardware.js
+++ frontend/src/pages/profile/components/ProfileHardware.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react'
     render() {
         return (
             <div>
-                <div id="ProfileHardware" class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-                    <h1 class="h2">Hardware</h1>
+                <div id="ProfileHardware" className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+                    <h1 className="h2">Hardware</h1>
                 </div>
             </div>
         )
